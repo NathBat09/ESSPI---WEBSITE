75c59c04e90fa1af88365be624b0c330c154f52d
NathBat09 <natanael.batista@upr.edu>
2024-11-03 12:40:17 -0500

    s

--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -15,33 +15,34 @@ router.post("/register", validInfo, async (req, res) => {
   const { email, name, password } = req.body;
 
   try {
-    // Verificar si el usuario ya existe
+    // Check if the user already exists
     const user = await pool.query("SELECT * FROM users WHERE user_email = $1", [email]);
 
     if (user.rows.length > 0) {
       return res.status(401).json("User already exists!");
     }
 
-    // Generar un hash de la contraseña antes de almacenarla
+    // Generate a hash of the password before storing it
     const salt = await bcrypt.genSalt(10);
     const bcryptPassword = await bcrypt.hash(password, salt);
 
-    // Insertar nuevo usuario en la base de datos
+    // Insert new user into the database
     let newUser = await pool.query(
       "INSERT INTO users (user_name, user_email, user_password) VALUES ($1, $2, $3) RETURNING *",
       [name, email, bcryptPassword]
     );
 
-    // Generar un token JWT para el nuevo usuario
+    // Generate a JWT for the new user
     const jwtToken = jwtGenerator(newUser.rows[0].user_id);
 
-    // Responder con el token JWT
+    // Respond with the JWT
     return res.json({ jwtToken });
   } catch (err) {
-    console.error(err.message);
+    console.error("Registration Error:", err); // More detail in logs
     res.status(500).send("Server error");
   }
 });
+
 
 /**
  * Ruta POST para iniciar sesión.
