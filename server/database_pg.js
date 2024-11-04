5628384233d5512d600b62e812d14190056f96bc
NathBat09 <natanael.batista@upr.edu>
2024-11-04 12:08:47 -0500

    s

--- server/database_pg.js
+++ server/database_pg.js
@@ -1,13 +1,14 @@
+require('dotenv').config(); // Load environment variables from .env
+
+const { Pool } = require("pg"); // Import Pool from pg
+
+// Initialize the Pool instance with the database connection details
 const pool = new Pool({
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     host: process.env.DATABASE_HOST,
     port: process.env.DATABASE_PORT,
     database: process.env.DATABASE_NAME
-});
-
-pool.on('error', (err) => {
-    console.error("Unexpected database error:", err);
 });
 
 module.exports = pool;
